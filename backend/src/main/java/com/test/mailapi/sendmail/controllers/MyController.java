d8e1b239e9e48a9509e2848c4a40b0646b31073d
Mohit Singh <msakra000@gmail.com>
2024-06-10 23:36:00 +0530

    first commit

---
+++ backend/src/main/java/com/test/mailapi/sendmail/controllers/MyController.java
@@ -0,0 +1,28 @@
+package com.test.mailapi.sendmail.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.mailapi.sendmail.model.EmailMessage;
+import com.test.mailapi.sendmail.services.MyService;
+
+@RestController
+public class MyController {
+    @Autowired
+    private  MyService myService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "This is home page;";
+    }
+    @PostMapping("/sendemail")
+    public ResponseEntity<?> sendMail(@RequestBody EmailMessage message) {
+        myService.sendEmail(message.getBody(), message.getSubject(), message.getTo());
+        System.out.println("Message: " + message);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ backend/src/main/java/com/test/mailapi/sendmail/model/EmailMessage.java
@@ -0,0 +1,40 @@
+package com.test.mailapi.sendmail.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailMessage {
+    private String to;
+    private String subject;
+    private String body;
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public EmailMessage(String to, String subject, String body) {
+        this.to = to;
+        this.subject = subject;
+        this.body = body;
+    }
+    public EmailMessage() {
+    }
+    @Override
+    public String toString() {
+        return "EmailMessage [to=" + to + ", subject=" + subject + ", body=" + body + "]";
+    }
+
+}
